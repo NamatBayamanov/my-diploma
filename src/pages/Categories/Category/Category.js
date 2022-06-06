6b549a88a842995d943210d499eb6cb1bf89a7a7
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-06 08:22:35 +0600

    corrected

--- src/pages/Categories/Category/Category.js
+++ src/pages/Categories/Category/Category.js
@@ -1,7 +1,7 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 // import CategoryList from "../../../components/CategoryList/CategoryList";
 import SubCategoryList from "../../../components/SubCateryList/SubCategoryList";
-import { getCategory } from "../../../data/Categories";
+// import { getCategory } from "../../../data/Categories";
 import { getSubCategories } from "../../../data/SubCategories";
 // import Cases from "../../Cases/Cases";
 // import SubCategories from "../../SubCategories/SubCategories";
@@ -12,9 +12,9 @@ import classes from "./Category.module.scss";
 function Category() {
   
 
-  const params = useParams();
+  // const params = useParams();
 
-  const category = getCategory(params.categoryId);
+  // const category = getCategory(params.categoryId);
   return ( 
     <>
       <div className={classes.Category}>
