2e4ac2e479bf51236873cb0809667eff6eab0b58
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-22 14:06:55 +0600

    added redux

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,46 @@
 import classes from "./Footer.module.scss";
-
+import footerImg from "../../assets/delivery/footerimg.svg";
 
 function Footer() {
-  return ( 
+  return (
     <>
       <div className={classes.Footer}>
-        <div className="container">
-          <div className={classes.footer__flex}>
-            Smartphones from 2022
+        <div className={classes.background}>
+          <div className="container">
+            <div>
+              <div className={classes.content}>
+                <div className={classes.footer__flex}>
+                  <article>
+                    <h2>Subscribe to news</h2>
+                    <p className={classes.footer__size}>
+                      Be the first to know about new products and special offers
+                    </p>
+                  </article>
+
+                  <div className={classes.form}>
+                    <form>
+                      <input type="text" placeholder="Email*" />
+                      <button>Subscribe</button>
+                    </form>
+                  </div>
+                </div>
+
+                <div className={classes.footer__flex}>
+                  <div className={classes.img__container}>
+                    <img alt="#" src={footerImg} />
+                  </div>
+                  <article>
+                    <p>+996 709 266 729</p>
+                  </article>
+                </div>
+                <footer>
+                  <p>
+                    2017-2021 © Mobila.shop Online store for smartphones and
+                    accessories. All rights reserved.
+                  </p>
+                </footer>
+              </div>
+            </div>
           </div>
         </div>
       </div>
