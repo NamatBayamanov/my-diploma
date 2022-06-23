08d11fc8973d8efe3e0c971f00fcaf72384bb694
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-23 15:09:45 +0600

    added components

--- src/components/CarLink/CarLink.js
+++ src/components/CarLink/CarLink.js
@@ -1,12 +1,22 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function CartLink() {
-  const number = useSelector(store => {
-    return Object.values(store.cart.items).reduce((sum, number) => sum + number, 0);
+  const number = useSelector((store) => {
+    return Object.values(store.cart.items).reduce(
+      (sum, number) => sum + number,
+      0
+    );
   });
 
   return (
-    <NavLink to="/cart">Cart ({number})</NavLink>
+    <div>
+      <NavLink to="/cart">
+        <FaShoppingCart /> <span>
+        ({number})
+        </span>
+      </NavLink>
+    </div>
   );
 }
--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-
+import classes from "./CartButton.module.scss";
 export default function CartButton({ productId }) {
 
   console.log(productId);
@@ -11,6 +11,6 @@ export default function CartButton({ productId }) {
   }
 
   return (
-    <button className="CartButton" onClick={onAddToCart}>Add to card</button>
+    <button className={classes.CartButton} onClick={onAddToCart}>Add to cart</button>
   );
 }
