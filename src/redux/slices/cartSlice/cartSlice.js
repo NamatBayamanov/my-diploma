0281c1ae93763002cf2bf6874d13bf753f63af16
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-23 15:10:06 +0600

    added styles

--- src/redux/slices/cartSlice/cartSlice.js
+++ src/redux/slices/cartSlice/cartSlice.js
@@ -1,4 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const checkout = createAsyncThunk('cart/checkout', async (order, thunkAPI) => {
+  const response = await axios.post('https://redux-router-230f1-default-rtdb.firebaseio.com/products.json', order);
+
+  return response.data;
+});
+
+function sameInStorage(items) {
+  localStorage.setItem("cartItems", JSON.stringify(items));
+}
 
 const cartSlice = createSlice({
   name: "cart",
@@ -14,12 +25,17 @@ const cartSlice = createSlice({
       else {
         store.items[action.payload] = 1;
       }
+      sameInStorage(store.items);
     },
     delete: (store, action) => {
       delete store.items[action.payload];
+      sameInStorage(store.items);
+
     },
     increment: (store, action) => {
       store.items[action.payload]++;
+      sameInStorage(store.items);
+
     },
     decrement: (store, action) => {
       if (store.items[action.payload] > 1) {
@@ -28,10 +44,20 @@ const cartSlice = createSlice({
       else {
         delete store.items[action.payload];
       }
-    },
-    
+      sameInStorage(store.items);
 
-  }
+    },
+    restore: (store, action) => {
+      store.items = JSON.parse(localStorage.getItem("cartItems") ?? `{}` );
+    },
+
+  },
+  extraReducers: {
+    [checkout.fulfilled]: (state, action) => {
+      console.log(action);
+      state.items = {};
+    }
+  },
 });
 
 export default cartSlice.reducer;
