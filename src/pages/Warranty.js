fada29568dadf5e8affcfb3ee679367e33d1a736
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-25 19:39:54 +0600

    added new page

---
+++ src/pages/Warranty.js
@@ -0,0 +1,11 @@
+import WarrantyView from "../components/WarrantyView/WarrantyView";
+
+function Warranty() {
+  return ( 
+    <>
+      <WarrantyView/>
+    </>
+  );
+}
+
+export default Warranty;
