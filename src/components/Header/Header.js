822b7a0ebea64df569c789490eabd6577be68060
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-25 19:36:33 +0600

    added changes in styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,36 +12,91 @@ import classes from "./Header.module.scss";
 
 
 
-function Header({image, children, title, text, image2, image3}) {
+// function Header({image, children, title, text, image2, image3}) {
 
 
-  return ( 
-    <>  
-      <div className={classes.Header}>
-        <div className={classes.background}>
-          <div className="container">
-            <article className={classes.content}>
-              <div className={classes.img__container}>
-                <img src={image} alt="#"/>
-              </div>
-              <p>
-                <p>
-                {children}
-                </p>
-                <span>
-                  &nbsp;
-                </span>
-                <aside>
+//   return ( 
+//     <>  
+//       <div className={classes.Header}>
+//         <div className={classes.background}>
+//           <div className="container">
+//             <article className={classes.content}>
+//               <div className={classes.img__container}>
+//                 <img src={image} alt="#"/>
+//               </div>
+//               <p>
+//                 <p>
+//                 {children}
+//                 </p>
+//                 <span>
+//                   &nbsp;
+//                 </span>
+//                 <aside>
                   
-                </aside>
-              </p>
-            </article>
+//                 </aside>
+//               </p>
+//             </article>
 
+//           </div>
+//         </div>
+//       </div>
+
+
+
+//     </>
+//   );
+// }
+
+// export default Header;
+
+
+
+
+function Header() {
+  let cards = [
+    {
+      title: "Mountain View",
+      copy: "Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains",
+      button: "View Trips",
+    },
+    {
+      title: "To The Beach",
+      copy: "Plan your next beach trip with these fabulous destinations",
+      button: "View Trips",
+    },
+    {
+      title: "Desert Destinations",
+      copy: "It's the desert you've always dreamed of",
+      button: "Book Now",
+    },
+    {
+      title: "Explore The Galaxy",
+      copy: "Seriously, straight up, just blast off into outer space today",
+      button: "Book Now",
+    },
+  ];
+
+  const cards2 = cards.slice(0, 1).map((card) => {
+    return (
+      <div className={classes.Header}>
+        <div className={classes.page_content}>
+          <div className={classes.card}>
+            <div className={classes.content}>
+              <h2 className={classes.title}></h2>
+              <p className={classes.copy}></p>
+              {/* <div>
+                <button className={classes.btn}>{card.button}</button>
+              </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </>
-  );
+    );
+  });
+
+  const cards3 = cards2;
+
+  return <div>{cards3}</div>;
 }
 
 export default Header;
