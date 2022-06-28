8bf8a2fa95af1cdc6f73fc285966299993b433e7
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-28 13:09:22 +0600

    added change

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,38 @@
-export default function Auth() {
-  return (
-      <form>
-          <label>
-              Email:
-              <input type="email" name="email" />
-          </label>
-          <label>
-              Password:
-              <input type="password" name="password" />
-          </label>
 
-          <button>Sign in</button>
-          <button>Sign up</button>
-      </form>
-  );
+import { useDispatch } from "react-redux";
+import { start } from "../redux/slices/authSlice/authSlice";
+
+export default function Auth() {
+
+    const dispatch = useDispatch();
+
+    function onAuthStart(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    dispatch(
+        start({
+        email: formData.get("email"),
+        password: formData.get("password"),
+        method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
+        })
+    );
+}
+    return (
+    <form onSubmit={onAuthStart}>
+        <label>
+        Email:
+        <input type="email" name="email" />
+        </label>
+        <label>
+        Password:
+        <input type="password" name="password" />
+        </label>
+
+        <button>Sign in</button>
+        <br/>
+        <br/>
+        <button>Sign up</button>
+    </form>
+    );
 }
