c5d0cb15c2dccae53292f5b97319ec7f55aa61bd
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-28 13:08:56 +0600

    added changes

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -1,16 +1,16 @@
 import { useDispatch } from "react-redux";
-import { add, add2 } from "../../redux/slices/cartSlice/cartSlice";
+import { add } from "../../redux/slices/cartSlice/cartSlice";
 import classes from "./CartButton.module.scss";
 export default function CartButton({ productId, subCategoryId }) {
 
   console.log(productId);
-  console.log(subCategoryId);
+  // console.log(subCategoryId);
 
   const dispatch = useDispatch();
 
   function onAddToCart() {
     // dispatch({ type: 'cart/add', payload: productId }); 
-    dispatch(add(`${productId} ${subCategoryId}`));
+    dispatch(add(productId));
     // dispatch(add2(subCategoryId));
   }
 
