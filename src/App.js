dd27d669993282099e17fe490d418d1487f85f72
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-28 13:11:05 +0600

    added changes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,34 @@ import Warranty from "./pages/Warranty";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Products/Product/Product";
 import Category from "./pages/Category/Category";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 // import Cases from "./pages/Cases/Cases";
 import SubCategory from "./pages/SubCategory/SubCategory";
 import Cart from "./pages/Cart";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CheckOut from "./pages/CheckOut";
 import Auth from "./pages/Auth";
 import Categories from "./pages/Categories/Categories";
+import Subcategoryy from "./pages/Subcategoryy";
+import { getCategory } from "./data/Categories";
+import { getSubCategory } from "./data/SubCategories";
 // import SubCategories from "./pages/SubCategories/SubCategories";
 
+
 function App() {
+
+  const params = useParams();
+
+  console.log(params)
+
+  const category = getCategory(params.categoryid);
+
+  const id = {...category}
+
+  const subCategory = getSubCategory();
+
+  
 
   const dispatch = useDispatch();
 
@@ -41,9 +57,23 @@ function App() {
             <Route path="/products/:productId" element={<Product/>}/>
             
             <Route path="/categories" element={<Categories/>}/>
+
+
+
+
+
             <Route path="/categories/:categoryId" element={<Category/>}/>
 
-            <Route path="/:subCategoryId" element={<SubCategory/>}/>
+            <Route path={`/categories/category/:id`} element={<SubCategory />}/>
+
+
+
+
+
+
+
+
+
 
             <Route path="headphones" element={<Headphones/>}/>
 
