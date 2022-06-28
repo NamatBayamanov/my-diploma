33057f36e0485f22ceea88dc1461f1d2bd68e452
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-28 13:09:55 +0600

    added styles

--- src/pages/SubCategory/SubCategory.js
+++ src/pages/SubCategory/SubCategory.js
@@ -1,10 +1,36 @@
+// import { useState } from "react";
+import { useParams } from "react-router-dom";
+import CartButton from "../../components/CartButton/CartButton";
+import { getSubCategory } from "../../data/SubCategories";
 import classes from "./SubCategory.module.scss";
 
-
 function SubCategory() {
-  return ( 
+  const params = useParams();
+  console.log(params);
+
+  const subCategory = getSubCategory(params.id);
+
+  console.log(subCategory)
+
+  return (
     <div className={classes.SubCategory}>
-        adfadfdadff
+      <div className={classes.background}>
+        <div className="container">
+          
+          <div className={classes.content}>
+            <div className={classes.img__container}>
+              <img width="391" height="500" src={subCategory.image} />
+              <div className={classes.backdrop}></div>
+
+              <h2>{subCategory.title}</h2>
+              <p>{subCategory.description}</p>
+              <div className={classes.cartbtn}>
+                <CartButton subCategoryId={subCategory.subCategoryId}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
