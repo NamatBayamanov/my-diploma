d03d4f7d2b03e0c4892c948289893feecbbe7ddb
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-29 15:25:14 +0600

    added changes

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,59 +1,75 @@
-
 import { useDispatch } from "react-redux";
 import { start } from "../redux/slices/authSlice/authSlice";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import classes from "./Auth.module.scss";
 export default function Auth() {
-
     const dispatch = useDispatch();
 
-    const { error, localId } = useSelector(store => store.auth);
-
+    const { error, localId } = useSelector((store) => store.auth);
 
     const navigate = useNavigate();
 
     function onAuthStart(event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    const formData = new FormData(event.target);
-    dispatch(
-        start({
-        email: formData.get("email"),
-        password: formData.get("password"),
-        method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
-        })
-    );
+        const formData = new FormData(event.target);
+        dispatch(
+            start({
+                email: formData.get("email"),
+                password: formData.get("password"),
+                method:
+                    event.nativeEvent.submitter.innerText === "Sign up"
+                        ? "signup"
+                        : "signin",
+            })
+        );
 
-    // navigate('/');
-}
+        // navigate('/');
+    }
 
+    if (localId !== null) {
+        navigate("/");
+    }
 
-if (localId !== null) {
-    navigate('/');
-}
-
-let errorOutput = null;
+    let errorOutput = null;
     if (error) {
-    errorOutput = <strong style={{ color: "red" }}>{error}</strong>
-}
-
+        errorOutput = <strong style={{ color: "red" }}>{error}</strong>;
+    }
 
     return (
-    <form onSubmit={onAuthStart}>
-        {errorOutput}
-        <label>
-        Email:
-        <input type="email" name="email" />
-        </label>
-        <label>
-        Password:
-        <input type="password" name="password" />
-        </label>
+        <div className={classes.Auth}>
+            <div className={classes.backdrop}>
+                
+                    <div className={classes.content}>
+                    <div className="container">
+                <div className={classes.error}>
+                            {errorOutput}
+                            </div>
+                        <form onSubmit={onAuthStart}>
+                            
+                            
+                            <label>
+                                Email:
+                                <input type="email" name="email" />
+                            </label>
+                            <label>
+                                Password:
+                                <input type="password" name="password" />
+                            </label>
 
-        <button>Sign in</button>
-        <br/>
-        <br/>
-        <button>Sign up</button>
-    </form>
+                            <div className={classes.for__btn}>
+                            <button className={classes.for__btn_1}>Sign in</button>
+                            <br />
+                            <br />
+                            <button className={classes.for__btn_2}  >Sign up</button>
+                            </div>
+
+                        
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
