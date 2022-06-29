21d1a4a871080bc703879c57fdc6345011b07334
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-29 09:43:05 +0600

    added colossal changes

--- src/data/Products.js
+++ src/data/Products.js
@@ -10,6 +10,24 @@ import iphone12ProMaxImage from "../assets/iphone12promax.jpg";
 import huaweiP40ProImage from "../assets/huaweip40pro.jpg";
 import honor30Image from "../assets/honor30.jpg";
 import googlePixel4xlImage from "../assets/googlepixel4.jpg";
+import iImage from "../assets/charging/i.webp";
+// --------
+import bumperImage from "../assets/bamper_case.jpg";
+import coverImage from "../assets/cover_pad.jpg";
+import bookImage from "../assets/cover_book.jpg";
+import flipImage from "../assets/flip_case.jpg";
+import rubberImage from "../assets/rubber_case.jpg";
+import texttileImage from "../assets/texttile_case.jpg";
+import hImage from "../assets/earphones/h.webp";
+import sImage from "../assets/earphones/s.webp";
+import eImage from "../assets/earphones/e.webp";
+import tImage from "../assets/stickers/t.webp";
+import cImage from "../assets/charging/c.webp";
+
+import chargingImage from "../assets/charging/ch.webp";
+import stickersImage from "../assets/secureglass/s.webp";
+import headphonesImage from "../assets/headphones/headyandex.webp";
+import casesImage from "../assets/cases/y.webp";
 
 
 
@@ -38,7 +56,6 @@ const products = [
 
     description: "Released 2020, June 02 192g, 8.9mm thickness Android 10, up to Android 11, One UI 3.1 32GB/64GB/128GB storage, microSDXC 163.7 x 75.3 x 8.9 mm (6.44 x 2.96 x 0.35 in) 192 g (6.77 oz) Glass front, plastic frame, plastic back Single SIM (Nano-SIM) or Dual SIM (Nano-SIM, dual stand-by) PLS LCD 6.5 inches, 102.0 cm2 (~82.8% screen-to-body ratio) 720 x 1600 pixels, 20:9 ratio (~270 ppi density) Android 10, upgradable to Android 11, One UI 3.1 Exynos 850 (8nm) Octa-core (4x2.0 GHz Cortex-A55 & 4x2.0 GHz Cortex-A55) Mali-G52 microSDXC (dedicated slot) 32GB 2GB RAM, 32GB 3GB RAM, 32GB 4GB RAM, 64GB 3GB RAM, 64GB 4GB RAM, 64GB 6GB RAM, 128GB 3GB RAM, 128GB 4GB RAM, 128GB 6GB RAM eMMC 5.1",
   },
-
   {
     action: "Action..!",
     image: redmiNote11Image,
@@ -63,8 +80,6 @@ const products = [
     price: "1235",
     cart: <BsCart2/>,
   },
-
-
   {
     
     action: "Action..!",
@@ -90,8 +105,6 @@ const products = [
 
     description: "Released 2019, February 27 162g, 8.4mm thickness Android 9.0 64GB storage, microSDXC 156 x 68 x 8.4 mm (6.14 x 2.68 x 0.33 in) 162 g (5.71 oz) Glass front (Gorilla Glass 5), plastic back, plastic frame Single SIM (Nano-SIM) or Hybrid Dual SIM (Nano-SIM, dual stand-by) IPS LCD 6.0 inches, 84.1 cm2 (~79.3% screen-to-body ratio)	1080 x 2520 pixels, 21:9 ratio (~457 ppi density)	Corning Gorilla Glass 5  Android 9.0 (Pie)	Qualcomm SDM630 Snapdragon 630 (14 nm) Octa-core 2.2 GHz Cortex-A53	Adreno 508",
   },
-
-
   {
     action: "Action..!",
     image: realmec11Image,
@@ -115,8 +128,6 @@ const products = [
 
     description: "Released 2020, Jule 07 196g, 9.1mm thickness Android 10, Realme UI 32GB storage, microSDXC 164.4 x 75.9 x 9.1 mm (6.47 x 2.99 x 0.36 in) 196 g (6.91 oz)	Dual SIM (Nano-SIM, dual stand-by) IPS LCD 6.5 inches, 103.7 cm2 (~83.1% screen-to-body ratio) 720 x 1560 pixels, 19.5:9 ratio (~264 ppi density) Android 10, Realme UI MediaTek MT6765G Helio G35 (12 nm) Octa-core (4x2.3 GHz Cortex-A53 & 4x1.8 GHz Cortex-A53) PowerVR GE8320 microSDXC (dedicated slot) 32GB 2GB RAM, 32GB 3GB RAM eMMC 5.1",
   },
-
-
   {
     action: "Action..!",
     image: iphone12ProMaxImage,
@@ -140,8 +151,6 @@ const products = [
 
     description: "Released 2020, November 13 228g, 7.4mm thickness iOS 14.1, up to iOS 15.5 128GB/256GB/512GB storage, no card slot 160.8 x 78.1 x 7.4 mm (6.33 x 3.07 x 0.29 in) 228 g (8.04 oz) Glass front (Gorilla Glass), glass back (Gorilla Glass), stainless steel frame Single SIM (Nano-SIM and/or eSIM) or Dual SIM (Nano-SIM, dual stand-by) - for China IP68 dust/water resistant (up to 6m for 30 mins) Apple Pay (Visa, MasterCard, AMEX certified) Super Retina XDR OLED, HDR10, Dolby Vision, 800 nits (HBM), 1200 nits (peak) 6.7 inches, 109.8 cm2 (~87.4% screen-to-body ratio) 1284 x 2778 pixels, 19.5:9 ratio (~458 ppi density) Scratch-resistant ceramic glass, oleophobic coating Wide color gamut True-tone  OS 14.1, upgradable to iOS 15.5 Apple A14 Bionic (5 nm) Hexa-core (2x3.1 GHz Firestorm + 4x1.8 GHz Icestorm) Apple GPU (4-core graphics) Card slot	No 128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM NVMe",
   },
-
-
   {
     action: "Action..!",
     image: huaweiP40ProImage,
@@ -166,8 +175,6 @@ const products = [
 
     description: "Size: 6.58 inches *With a rounded corners design on the display, the diagonal length of the screen is 6.58 inches when measured according to the standard rectangle (the actual viewable area is slightly smaller). Colour: 16.7 million colours Type: OLED, up to 90 Hz frame refresh rate Resolution: 2640 x 1200 Pixels  *The resolution measured as a standard rectangle, with a rounded corners design, the effective pixels are slightly less. HUAWEI Kirin 990 5G CPU: Octa-core 2 x Cortex-A76 Based 2.86 GHz + 2 x Cortex-A76 Based 2.36 GHz + 4 x Cortex-A55 1.95 GHz GPU: 16-Core Mali-G76 NPU: Dual Big Core + Tiny Core NPUs (Neural-network Processing Unit) 8 GB RAM + 256 GB ROM *The available internal storage may be smaller as part of the internal storage is occupied by software. Actual memory space may change due to application updates, user operations, and other related factors. External Memory Support: NM SD Card, up to 256 GB *sold separately",
   },
-
-
   {
     action: "Action..!",
     image: honor30Image,
@@ -192,10 +199,8 @@ const products = [
 
     description: "Honor 30 - 6.53 inches, 20:9, 2400 x 1080 pixels, OLED. Honor 30 Pro - 6.57 inches, 19.5:9, 2340 x 1080 pixels, OLED, 90 Hz (for Honor 30 Pro +) Honor 30 - Huawei Kirin 985, 8 cores, 2.58 GHz, 7 nm, GPU Mali-G77. Honor 30 Pro - Huawei Kirin 990 5G, 8 cores, 2.86 GHz, 7 nm, GPU Mali-G76MP16 Honor 30 - operational 6/8 GB, built-in 128/256 GB, a separate slot for a Nano Memory card. Honor 30 Pro - operational 8/12 GB, built-in 128/256 GB, a separate slot for a Nano Memory card Honor 30 - 160.3 x 74.2 x 8.1mm, 185g Honor 30 Pro - 160.3 x 73.6 x 8.4, 186g (190g Honor 30 Pro+) 5G, LTE, Wi-Fi 802.11 a/b/g/n/ac/ax, Bluetooth 5.1, NFC, GPS, A-GPS, NFC, USB-C, OTG",
   },
-
-
   {
-    action: "Action..!",
+    
     image: googlePixel4xlImage,
     title: "Google Pixel 4 XL",
     rating1: ratingImage,
@@ -204,11 +209,13 @@ const products = [
     rating4: ratingImage2,
     rating5: ratingImage2,
     grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
 
     productId: "google_pixel_4xl",
 
-    price: "849",
-    cart: <BsCart2/>,
+  
 
 
     shortInformation: "(Short information)",
@@ -218,7 +225,304 @@ const products = [
 
     description: "Released 2019, October 22 193g, 8.2mm thickness Android 10, up to Android 12 64GB/128GB storage, no card slot 160.4 x 75.1 x 8.2 mm (6.31 x 2.96 x 0.32 in) 193 g (6.81 oz) Glass front (Gorilla Glass 5), glass back (Gorilla Glass 5), aluminum frame Nano-SIM and/or eSIM IP68 dust/water resistant (up to 1.5m for 30 mins) P-OLED, 90Hz, HDR 6.3 inches, 98.0 cm2 (~81.3% screen-to-body ratio) 1440 x 3040 pixels, 19:9 ratio (~537 ppi density) Corning Gorilla Glass 5 Always-on display Android 10, upgradable to Android 12 Qualcomm SM8150 Snapdragon 855 (7 nm) Octa-core (1x2.84 GHz Kryo 485 & 3x2.42 GHz Kryo 485 & 4x1.78 GHz Kryo 485) Adreno 640 64GB 6GB RAM, 128GB 6GB RAM",
   },
+  {
+    title: "Bumper case ",
+    image: bumperImage,
+    productId: "bumper_case",
+    description: "Bumper cases are made of shock-absorbing materials, protecting phone against bumps. They are good alternatives to traditional cases, because you can show off the design and colours of your smartphones.",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Cover-pad",
+    image: coverImage,          
+    productId: "cover_pad",
+
+    description: "Cases, which are designed to attach to, support, or otherwise hold a smartphone, are popular accessories. Case measures are based on the display inches (e.g. 5 inch display)",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Case book",
+    image: bookImage,
+    productId: "case_book",
+
+    description: "For overall device protection, a case, which covers the corners, edges and back of a smartphone, is your best bet. A good case will protect your phone from scratches and absorb impact in those areas when your device is dropped",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Flip case",
+    image: flipImage,
+    productId: "flip_case",
+
+    description: "Find premium phone cases and covers to protect your smartphone for a lifetime of use",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Rubber",
+    image: rubberImage,
+    productId: "rubber_case",
+
+    description: "Cases, which are designed to attach to, support, or otherwise hold a smartphone, are popular accessories. Case measures are based on the display inches (e.g. 5 inch display)",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    action: "Action..!",
+    title: "Inductive charging",
+    image: iImage,
+    productId: "inductive charging",
+    description: "Inductive charging (also known as wireless charging or cordless charging) is a type of wireless power transfer",
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Noise-cancelling",
+    image: hImage,
+    productId: "noise-cancelling",
+    description: "Noise-cancelling headphones reduce unwanted ambient sounds using active noise control. This is distinct from passive headphones which, if they reduce ambient sounds at all, use techniques such as soundproofing Noise cancellation makes it possible to listen to audio content without raising the volume excessively. It can also help a passenger sleep in a noisy vehicle such as an airliner.",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Supra-aural",
+    image: sImage,
+    productId: "supra-aural",
+    description: "Supra-aural headphones are headphones with ear cups designed to rest on your ears instead of around your ears.You may also know supra-aural headphones as on-ear headphones.",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Earbud",
+    image: eImage,
+    productId: "earbud",
+    description: "Earphones (also called in-ear headphones or in-ears) are inserted into the ear canal, and earbuds rest outside the ear canal.",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "For good a mood",
+    image: tImage,
+    productId: "mood",
+    description: "It is our private sticker for you to memorise us with respectfully",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Charging",
+    image: cImage,
+    productId: "charging",
+    description: "A trickle charger provides a relatively small amount of current, only enough to counteract self-discharge of a battery that is idle for a long time.",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+  {
+    title: "Textile",
+    image: texttileImage,
+    productId: "texttile_case",
+
+    description: "For overall device protection, a case, which covers the corners, edges and back of a smartphone, is your best bet. A good case will protect your phone from scratches and absorb impact in those areas when your device is dropped",
+
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "53",
+    action: "Action..!",
+    price: "849",
+    cart: <BsCart2/>,
+  },
+
+  {
+    action: "Novelty",
+    image: chargingImage,
+    title: "Fast charging",
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "71",
+
+    productId: "powerful_chargings",
+
+    price: "125",
+    cart: <BsCart2/>,
+
+
+    shortInformation: "(Short information)",
+
+    buy: "Buy...",
+
+
+    description: "Fast Charging is a term frequently used to market chargers and devices capable of charging faster than the current charging standard (5 Watts).",
+  },
+
+  {
+    action: "Novelty",
+    image: stickersImage,
+    title: "Protective glass",
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "71",
+
+    productId: "protective-glass",
+
+    price: "23",
+    cart: <BsCart2/>,
+
+
+    shortInformation: "(Short information)",
+
+    buy: "Buy...",
+
+
+    description: "A screen protector is an additional sheet of material—commonly polyurethane or laminated glass—that can be attached to the screen of an electronic device and protect it against physical damage.",
+  },
+
+  {
+    novelty: "Novelty",
+    image: headphonesImage,
+    title: "Comfortable headphone",
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "86",
+
+    productId: "comfortable_headphone",
+
+    price: "499",
+    cart: <BsCart2/>,
+
+
+    shortInformation: "(Short information)",
+
+    buy: "Buy...",
+
+
+    description: "Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears. They are electroacoustic transducers, which convert an electrical signal to a corresponding sound.",
+  },
+
+  {
+    novelty: "Novelty",
+    image: casesImage,
+    title: "Leather case",
+    rating1: ratingImage,
+    rating2: ratingImage,
+    rating3: ratingImage,
+    rating4: ratingImage2,
+    rating5: ratingImage2,
+    grade: "74",
+    productId: "case_for_smartphones",
+
+    price: "84",
+    cart: <BsCart2/>,
+
+
+    shortInformation: "(Short information)",
+
+    buy: "Buy...",
+
+
+    description: "Smartphone cases are the most popular accessories for smartphones. They protect phones from scratches, dings, and drops while adding a layer of style.",
+  },
 ];
+
 export function getProducts() {
   return products;
 };
