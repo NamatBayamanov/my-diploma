2cd2e15fa30abd09405441157a56f503044ec0c7
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-29 10:17:35 +0600

    created new component

---
+++ src/components/HomeView/HomeViewImg/HomeViewImg.js
@@ -0,0 +1,19 @@
+import classes from "./HomeViewImg.module.scss";
+
+function HomeViewImg({img}) {
+  return ( 
+    <div className={classes.HomeViewImg}>
+    <div className={classes.shadow}>
+              <section className={classes.welcome}>
+                Welcome!
+              </section>
+            <article className={classes.backdrop}></article>
+            <div className={classes.img__container}>
+              <img alt="#" src={img}/> 
+            </div>
+            </div>
+    </div>
+  );
+}
+
+export default HomeViewImg;
