51a035b864986974a24ad5aa95bc01cc62c67d24
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:51:52 +0600

    created new page

---
+++ src/pages/History.js
@@ -0,0 +1,11 @@
+import HistoryView from "../components/HistoryView/HistoryView";
+
+function History() {
+  return ( 
+    <div>
+        <HistoryView/>
+    </div>
+  );
+}
+
+export default History;
