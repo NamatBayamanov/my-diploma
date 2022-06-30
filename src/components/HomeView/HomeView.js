0443bdf25dc8312fdeb11241379107e0cc36f98a
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:47:20 +0600

    added changes

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -12,9 +12,17 @@ import viewImage4 from "../../assets/view4.jpg";
 
 import HomeViewImg from "./HomeViewImg/HomeViewImg";
 
-import cart from "../../assets/cart/cart.webp";
+// import cart from "../../assets/cart/cart.webp";
 // import CategoryList from "../CategoryList/CategoryList";
 // import { getCategories } from "../../data/Categories";
+import view1 from "../../assets/homeview/view1.webp";
+import view2 from "../../assets/homeview/view2.webp";
+import view3 from "../../assets/homeview/view3.webp";
+import view4 from "../../assets/homeview/view4.webp";
+import view5 from "../../assets/homeview/view5.webp";
+import view6 from "../../assets/homeview/view6.webp";
+import view7 from "../../assets/homeview/view7.webp";
+import view8 from "../../assets/homeview/view8.webp";
 
 
 
@@ -62,8 +70,14 @@ function HomeView() {
             
             {/* <CategoryList categories={getCategories()}/> */}
 
-            <HomeViewImg img={cart}/>
-            <HomeViewImg img={cart}/>
+            <HomeViewImg img={view1}>Welcome!</HomeViewImg>
+            <HomeViewImg img={view2}>Modern..!</HomeViewImg>
+            <HomeViewImg img={view3}>New Gen...</HomeViewImg>
+            <HomeViewImg img={view4}>Novelties...</HomeViewImg>
+            <HomeViewImg img={view5}>Qualitative...</HomeViewImg>
+            <HomeViewImg img={view6}>Warranty...</HomeViewImg>
+            <HomeViewImg img={view7}>Ficilities...</HomeViewImg>
+            <HomeViewImg img={view8}>Free Choise</HomeViewImg>
           </div>
         </div>
 
