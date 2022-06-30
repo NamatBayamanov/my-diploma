448237fd690a31eb066cc487f83e79cc146ff57c
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:45:00 +0600

    moved other place

---
+++ src/components/Category/Category.js
@@ -0,0 +1,37 @@
+// import { useParams } from "react-router-dom";
+// import CategoryList from "../../../components/CategoryList/CategoryList";
+import SubCategoryList from "../SubCateryList/SubCategoryList";
+// import { getCategory } from "../../../data/Categories";
+import { getSubCategories } from "../../data/SubCategories";
+// import Cases from "../../Cases/Cases";
+// import SubCategories from "../../SubCategories/SubCategories";
+// import Categories from "../Categories";
+import classes from "./Category.module.scss";
+
+
+function Category() {
+  
+
+  // const params = useParams();
+
+  // const category = getCategory(params.categoryId);
+  return ( 
+    <>
+      <div className={classes.Category}>
+        <div className={classes.background}>
+          <div className="container">
+            <div className={classes.content}>
+              {/* <SubCategories/> */}
+              <SubCategoryList subCategories={getSubCategories()}/>
+            </div>
+
+
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );    
+}
+
+export default Category;    
