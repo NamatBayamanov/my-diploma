7f504a78ead3fa420fae196772bce437fb09ff62
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:44:37 +0600

    added changes

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,23 @@
+import classes from "./Categories.module.scss";
+import { getCategories } from "../../data/Categories";
+import CategoryList from "../CategoryList/CategoryList";
+
+
+function Categories() {
+  return ( 
+    <>
+      <div className={classes.Categories}>
+        <div className={classes.background}>
+          <div className="container">
+            <div className={classes.content}>
+              <CategoryList categories={getCategories()}/>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Categories;
