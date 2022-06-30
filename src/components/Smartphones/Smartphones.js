9d5b4e5c4c2c540244c7dcd8604844aa16a98305
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:48:55 +0600

    added changes

---
+++ src/components/Smartphones/Smartphones.js
@@ -0,0 +1,20 @@
+import classes from  "./Smartphones.module.scss";
+
+
+function Smartphones() {
+  return ( 
+    <>
+      <div className={classes.Smartphones}>
+        <div className={classes.background}>
+          <div className="container">
+            <div className={classes.content}>
+              smart
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Smartphones;
