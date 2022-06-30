c68995b97e534a4b522d0cc1961cff690b392061
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:46:34 +0600

    added changes

---
+++ src/components/Headphones/Headphones.js
@@ -0,0 +1,25 @@
+import Header from "../../components/Header/Header";
+import classes from "./Headphones.module.scss";
+// import headphonesImage from "../../assets/headphones.jpg";
+
+
+function Headphones() {
+
+  return ( 
+    <>  
+      <div className={classes.Headphones}>
+        <div className={classes.background}>
+          <div className="container">
+            <div className={classes.content}>
+              <Header>
+              </Header>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Headphones;
