dc16fa5cb1a64b2609f9fcdaeaf04b08adbf4370
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:49:43 +0600

    created new page

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,75 +1,11 @@
-import { useDispatch } from "react-redux";
-import { start } from "../redux/slices/authSlice/authSlice";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import classes from "./Auth.module.scss";
-export default function Auth() {
-    const dispatch = useDispatch();
+import AuthView from "../components/AuthView/AuthView";
 
-    const { error, localId } = useSelector((store) => store.auth);
-
-    const navigate = useNavigate();
-
-    function onAuthStart(event) {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-        dispatch(
-            start({
-                email: formData.get("email"),
-                password: formData.get("password"),
-                method:
-                    event.nativeEvent.submitter.innerText === "Sign up"
-                        ? "signup"
-                        : "signin",
-            })
-        );
-
-        // navigate('/');
-    }
-
-    if (localId !== null) {
-        navigate("/");
-    }
-
-    let errorOutput = null;
-    if (error) {
-        errorOutput = <strong style={{ color: "red" }}>{error}</strong>;
-    }
-
-    return (
-        <div className={classes.Auth}>
-            <div className={classes.backdrop}>
-                
-                    <div className={classes.content}>
-                    <div className="container">
-                <div className={classes.error}>
-                            {errorOutput}
-                            </div>
-                        <form onSubmit={onAuthStart}>
-                            
-                            
-                            <label>
-                                Email:
-                                <input type="email" name="email" />
-                            </label>
-                            <label>
-                                Password:
-                                <input type="password" name="password" />
-                            </label>
-
-                            <div className={classes.for__btn}>
-                            <button className={classes.for__btn_1}>Sign in</button>
-                            <br />
-                            <br />
-                            <button className={classes.for__btn_2}  >Sign up</button>
-                            </div>
-
-                        
-                        </form>
-                    </div>
-                </div>
-            </div>
+function Auth() {
+    return ( 
+        <div>
+            <AuthView/>
         </div>
     );
 }
+
+export default Auth;
