1596b2868889a9e2d9357bffc60dcb68c8bd2170
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-30 11:47:28 +0600

    added changes

--- src/components/HomeView/HomeViewImg/HomeViewImg.js
+++ src/components/HomeView/HomeViewImg/HomeViewImg.js
@@ -1,15 +1,16 @@
 import classes from "./HomeViewImg.module.scss";
 
-function HomeViewImg({img}) {
+function HomeViewImg({img, children}) {
   return ( 
     <div className={classes.HomeViewImg}>
     <div className={classes.shadow}>
-              {/* <section className={classes.welcome}>
-                Welcome!
-              </section> */}
+    <section className={classes.welcome}>
+                {children}
+              </section>
             <article className={classes.backdrop}></article>
             <div className={classes.img__container}>
               <img alt="#" src={img}/> 
+            
             </div>
             </div>
     </div>
